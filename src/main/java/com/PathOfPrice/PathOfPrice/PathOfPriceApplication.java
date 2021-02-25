bfda51351b4b31b7d5cd8754a9b6acc1266600d9
jason.royer <jason.royer1@gmail.com>
2021-02-24 21:12:55 -0700

    start

---
+++ src/main/java/com/PathOfPrice/PathOfPrice/PathOfPriceApplication.java
@@ -0,0 +1,14 @@
+package com.PathOfPrice.PathOfPrice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PathOfPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathOfPriceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PathOfPrice/PathOfPrice/PathOfPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PathOfPrice.PathOfPrice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathOfPriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
